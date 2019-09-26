f9e633e1d015d3e93773fcd9daaaeda4a67207cb
Tanner Lancaster <saltillokid11@yahoo.com>
2019-09-26 09:15:50 -0400

    started part2

---
+++ practice/part2a/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Note from './components/Note'
+
+const App = ({ notes }) => {
+    // const { notes } = props
+  
+    const rows = () => notes.map(note => 
+      <Note 
+        key={note.id}
+        note={note}
+      />
+    )
+  
+    return (
+      <div>
+        <h1>Notes</h1>
+        <ul>
+          {rows()}
+        </ul>
+      </div>
+    )
+  }
+  
+  export default App
